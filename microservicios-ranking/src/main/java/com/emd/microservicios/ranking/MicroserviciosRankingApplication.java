c78c4caffbec5a288f8a3f0921280e7af871cfea
Edgardo Mora <edgardomora@hotmail.com>
2021-12-14 19:52:14 -0300

    agrega microservicio ranking, por desarrollar

---
+++ microservicios-ranking/src/main/java/com/emd/microservicios/ranking/MicroserviciosRankingApplication.java
@@ -0,0 +1,13 @@
+package com.emd.microservicios.ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosRankingApplication.class, args);
+	}
+
+}
